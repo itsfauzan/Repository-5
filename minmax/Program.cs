4d9db608f2587370701fecc4aa26f034bcbd6cc5
itsfauzan <fauzanyahya@students.amikom.ac.id>
2023-04-10 21:07:30 +0700

    commit5

---
+++ minmax/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace void_overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            number numbers = new number();
+            numbers.NilaiMin(95, 90);
+            numbers.NilaiMin(95, 90, 100);
+            numbers.NilaiMax(95, 90);
+            numbers.NilaiMax(95, 90, 100);
+        }
+    }
+}
